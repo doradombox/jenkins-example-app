2b1692d01bc2b7865be5de7db1cf66396f68b6e9
win10 <doradombox@outlook.com>
2024-08-20 15:06:22 +0800

    init

---
+++ src/main/java/priv/scor/edgar/jenkinsexampleapp/JenkinsExampleAppApplication.java
@@ -0,0 +1,13 @@
+package priv.scor.edgar.jenkinsexampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsExampleAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsExampleAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/priv/scor/edgar/jenkinsexampleapp/JenkinsExampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.scor.edgar.jenkinsexampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsExampleAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
